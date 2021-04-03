d3e5d826ecdbcbfa8a7fb4137a384cce9f84e94b
dreisdev <dyego.reis@outlook.com>
2021-04-03 15:24:47 -0300

    Development list - ListOf, sortedBy and groupBy.

---
+++ src/main/kotlin/one/digitalinnovation/collection/TesteList.kt
@@ -0,0 +1,37 @@
+package one.digitalinnovation.collection
+
+fun main(){
+    val joao = Funcionario ("João", 2000.0, "CLT" )
+    val pedro = Funcionario ("Pedro", 1500.0, "PJ")
+    val maria = Funcionario ("Maria", 4000.0, "CLT")
+
+    val funcionarios = listOf(joao, pedro, maria)
+
+    funcionarios.forEach { println(it) }
+
+    println("---------------------------")
+    println(funcionarios.find { it.nome =="Maria" })
+
+    println("---------------------------")
+    funcionarios.sortedBy { it.salario }
+        .forEach { println(it) }
+
+    println("---------------------------")
+    funcionarios.groupBy { it.tipodecontratacao }
+        .forEach { println(it) }
+}
+
+data class Funcionario(
+
+    val nome: String,
+    val salario: Double,
+    val tipodecontratacao: String
+)     {
+
+    override fun toString(): String =
+        """
+        Nome: $nome
+        Salário: $salario
+      
+    """.trimIndent()
+}
