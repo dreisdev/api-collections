0096695a4a2a2aae9d903aa3a9c278ba523ae23e
dreisdev <dyego.reis@outlook.com>
2021-04-05 21:59:49 -0300

    Created test mutable collections.

---
+++ src/main/kotlin/one/digitalinnovation/collection/TesteColecoesMutaveis.kt
@@ -0,0 +1,31 @@
+package one.digitalinnovation.collection
+
+fun main(){
+    val joao = Funcionario ("João", 2000.0, "CLT" )
+    val pedro = Funcionario ("Pedro", 1500.0, "PJ")
+    val maria = Funcionario ("Maria", 4000.0, "CLT")
+    println("----------List------------")
+    val funcionarios = mutableListOf(joao, maria)
+    funcionarios.forEach { println(it) }
+
+    println("----------------------")
+    funcionarios.add(pedro)
+    funcionarios.forEach { println(it) }
+
+    println("----------------------")
+    funcionarios.remove(joao)
+    funcionarios.forEach { println(it) }
+
+    println("----------Set------------")
+    val funcionariosSet = mutableSetOf(joao)
+    funcionariosSet.forEach { println(it) }
+
+    println("----------------------")
+    funcionariosSet.add(pedro)
+    funcionariosSet.add(maria)
+    funcionariosSet.forEach { println(it) }
+
+    println("----------------------")
+    funcionariosSet.remove(maria)
+    funcionariosSet.forEach { println(it) }
+}
